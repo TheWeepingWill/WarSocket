6108f8f089a966a25f9d543c35a6c640b83d561a
William Jones <williamseejones793@gmail.com>
2022-06-02 12:55:25 -0400

    added take_cards to warplayer

--- spec/war_game_spec.rb
+++ spec/war_game_spec.rb
@@ -2,7 +2,11 @@ require_relative '../lib/war_game'
 
 describe 'WarGame' do
     
-    # it "creates players" do 
-    # 	expect(WarGame.new.players).to
-    # end
+    it "creates players" do 
+    	expect(WarGame.new.players).not_to be_nil
+    end
+
+    it "has a deck" do 
+    	expect(WarGame.new.deck.count_cards).to eq(52)
+    end
 end
